e62a840c307d04f0e4b3d65905d2e560ed6f5cd3
Aishashirin <aishashirina@gmail.com>
2021-01-25 15:38:01 +0600

    add all methods in Controller

--- src/main/java/kz/attractorschool/moviereviewrr/Controller/Controller.java
+++ src/main/java/kz/attractorschool/moviereviewrr/Controller/Controller.java
@@ -6,8 +6,8 @@ import kz.attractorschool.moviereviewrr.model.User;
 import kz.attractorschool.moviereviewrr.service.MovieService;
 import kz.attractorschool.moviereviewrr.service.ReviewService;
 import kz.attractorschool.moviereviewrr.service.UserService;
-import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,8 +55,33 @@ public class Controller {
     }
 
     @GetMapping("/movies/byReleaseYear")
-    public List<Movie> findMovieByReleaseYear(@RequestParam("releaseYear") int year){
+    public List<Movie> findMovieByReleaseYear(@RequestParam("releaseYear") int year) {
         return movieService.findMovieByReleaseYear(year);
+    }
+
+    @GetMapping("/movies/byActor")
+    public List<Movie> findMoviesByActor(@RequestParam("actor") String name) {
+        return movieService.findMovieByActor(name);
+    }
+
+    @GetMapping("/movie/byRating")
+    public List<Movie> findMoviesByRating(@RequestParam("rating") double rating) {
+        return movieService.findMovieByRating(rating);
+    }
+
+    @GetMapping("/moviePage/sortByTitle")
+    public Page<Movie> pageSortByTitle() {
+        return movieService.sortByTitle();
+    }
+
+    @GetMapping("/moviePage/sortByRating")
+    public Page<Movie> pageSortByRating() {
+        return movieService.sortingByRating();
+    }
+
+    @GetMapping("/user/findByName")
+    public User findUserByName(@RequestParam("name") String name) {
+        return userService.findByName(name);
     }
 
 }
