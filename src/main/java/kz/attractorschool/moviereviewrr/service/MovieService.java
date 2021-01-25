53c3457add2bd66cfc89ec8d1f266d89dbb4f629
Aishashirin <aishashirina@gmail.com>
2021-01-25 14:13:00 +0600

    add sorting methods in MovieService

--- src/main/java/kz/attractorschool/moviereviewrr/service/MovieService.java
+++ src/main/java/kz/attractorschool/moviereviewrr/service/MovieService.java
@@ -12,22 +12,36 @@ import java.util.List;
 public class MovieService {
     private MovieRepository movieRepository;
 
-   public List<Movie> findMovieByTitle(String title) {
+    public List<Movie> findMovieByTitle(String title) {
         return movieRepository.findByTitle(title);
     }
 
-    public List<Movie> findMovieByReleaseYear(int year){
-       return movieRepository.findByReleaseYear(year);
+    public List<Movie> findMovieByReleaseYear(int year) {
+        return movieRepository.findByReleaseYear(year);
     }
 
-    public List<Movie> findMovieByActor(String name){
-       return movieRepository.findByActors(name);
+    public List<Movie> findMovieByActor(String name) {
+        return movieRepository.findByActors(name);
     }
 
-    public List<Movie> findMovieByRating(double rating){
-       return movieRepository.findByRating(rating);
+    public List<Movie> findMovieByRating(double rating) {
+        return movieRepository.findByRating(rating);
     }
 
+    public Page<Movie> sortByTitle() {
+        Sort sort = Sort.by(Sort.Order.asc("title"));
+        int page = 1;
+        int size = 3;
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return movieRepository.findAll(pageable);
+    }
 
+    public Page<Movie> sortingByRating() {
+        Sort sort = Sort.by(Sort.Order.desc("rating"));
+        int page = 1;
+        int size = 3;
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return movieRepository.findAll(pageable);
+    }
 }
 
